835c93660424bd5f61980c1137eec4aa6bd92b01
newbi462 <isthisemailfree@gmail.com>
2020-01-15 17:55:12 -0500

    validateUserId

--- server.js
+++ server.js
@@ -21,24 +21,9 @@ function logger(request, responce, next) {
 }
 
 //MOVE TO THE ROUTES FILE THAT HANDLES IDs
-//validateUserId()
-function validateUserId(request, responce, next) {
-//*validateUserId validates the user id on every request that expects a user id parameter
-//*if the id parameter is valid, store that user object as req.user
-  if ("id parameter" != undefined) {
-    request.user = "that user object";
-  }
-
-//*if the id parameter does not match any user id in the database, cancel the request and respond with status 400 and { message: "invalid user id" } validateUser()
-
-  next();
-}
 
 /*
-validateUser validates the body on a request to create a new user
-if the request body is missing, cancel the request and respond with status 400 and { message: "missing user data" }
-if the request body is missing the required name field, cancel the request and respond with status 400 and { message: "missing required name field" }
-validatePost()
+
 
 validatePost validates the body on a request to create a new post
 if the request body is missing, cancel the request and respond with status 400 and { message: "missing post data" }
--- users/userRouter.js
+++ users/userRouter.js
@@ -17,7 +17,7 @@ router.post('/', (req, res) => {
       console.log(error);
       res.status(500).json(
         {
-          error: "get() Could not GET and array of Useres from the DB."
+          error: "insert() Could Not Send your Reqest to DB."
         }
       )
     })
@@ -44,13 +44,13 @@ router.get('/', (req, res) => {
     })
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validateUserId, (req, res) => {
   // do your magic!
   const id = req.params.id;
   UserProjectData.getById(id)
     .then(usersObj => {
       res.status(200).json(usersObj);
-      //console.log(usersObj);
+      //console.log(typeof usersObj);
     })
     .catch( error => {
       console.log(error);
@@ -64,6 +64,20 @@ router.get('/:id', (req, res) => {
 
 router.get('/:id/posts', (req, res) => {
   // do your magic!
+  const id = req.params.id;
+  UserProjectData.getUserPosts(id)
+    .then(postsFromThisIDarr => {
+      res.status(200).json(postsFromThisIDarr);
+      console.log(postsFromThisIDarr);
+    })
+    .catch( error => {
+      console.log(error);
+      res.status(500).json(
+        {
+          error: "getUserPosts() Could not GET and array of posts from the DB."
+        }
+      )
+    })
 });
 
 router.delete('/:id', (req, res) => {
@@ -75,15 +89,55 @@ router.put('/:id', (req, res) => {
 });
 
 //custom middleware
-
+/*
+validateUserId()
+//*validateUserId validates the user id on every request that expects a user id parameter
+//*if the id parameter is valid, store that user object as req.user
+//*if the id parameter does not match any user id in the database, cancel the request and respond with status 400 and { message: "invalid user id" } validateUser()
+*/
 function validateUserId(req, res, next) {
   // do your magic!
-}
+  let userArr = 0;
+  console.log(req.params.id);
+  UserProjectData.getById(req.params.id)
+    .then(usersObj => {
+      console.log(usersObj);
+      userArr = usersObj;
+      console.log(typeof userArr == "object");
+    })
+    .catch( error => {
+      console.log(error);
+      res.status(500).json(
+        {
+          error: "validateUserId FAILED at getById."
+        }
+      )
+    })
+    console.log(typeof userArr == "object");
+  setTimeout(function () {
+    if (typeof userArr == "object") {
+      next();
+    } else {
+      res.status(400).json({ message: "invalid user id" });
+    }
+  }, 333);
+};
 
+/*
+validateUser validates the body on a request to create a new user
+if the request body is missing, cancel the request and respond with status 400 and { message: "missing user data" }
+if the request body is missing the required name field, cancel the request and respond with status 400 and { message: "missing required name field" }
+validatePost()
+*/
 function validateUser(req, res, next) {
   // do your magic!
 }
 
+/*
+validatePost validates the body on a request to create a new post
+if the request body is missing, cancel the request and respond with status 400 and { message: "missing post data" }
+if the request body is missing the required text field, cancel the request and respond with status 400 and { message: "missing required text field" }
+*/
 function validatePost(req, res, next) {
   // do your magic!
 }
