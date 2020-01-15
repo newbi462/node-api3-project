dba98ca667f1ad00da03de3b8fc57d6826d08264
newbi462 <isthisemailfree@gmail.com>
2020-01-15 14:54:54 -0500

    logger() middleware

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code away!
+//Dependancies
+const server = require('./server.js');
+
+// Import Data
+
+
+//START WITH EXPRESS
+
+
+//CUSTOME MILLEWHARE/HANDLE FUNCTIONS OR INPORTS OF
+
+
+//MIDDLE WARE
+
+
+//ROUTES
+
+
+//ENDPOINTS
+
+
+//LISTEN SERVER
+const port = 8000;
+server.listen(port, () => console.log((`\n ** api is be ups on: ${port} ** \n`)));
--- server.js
+++ server.js
@@ -1,13 +1,56 @@
-const express = require('express');
+//Dependancies
+const express = require("express");
+// Import Data
 
+
+//START WITH EXPRESS
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+//custom middleware
+/*
+logger()
+*logger logs to the console the following information about each request: request method, request url, and a timestamp
+*this middleware runs on every request made to the API
+*/
+function logger(request, responce, next) {
+  const { method, originalUrl } = request;
+  console.log(`${method} to ${originalUrl} at ${Date(Date.now())}`);
+
+  next();
+}
+
+
+
+/*
+validateUserId()
+
+validateUserId validates the user id on every request that expects a user id parameter
+if the id parameter is valid, store that user object as req.user
+if the id parameter does not match any user id in the database, cancel the request and respond with status 400 and { message: "invalid user id" }
+validateUser()
+
+validateUser validates the body on a request to create a new user
+if the request body is missing, cancel the request and respond with status 400 and { message: "missing user data" }
+if the request body is missing the required name field, cancel the request and respond with status 400 and { message: "missing required name field" }
+validatePost()
+
+validatePost validates the body on a request to create a new post
+if the request body is missing, cancel the request and respond with status 400 and { message: "missing post data" }
+if the request body is missing the required text field, cancel the request and respond with status 400 and { message: "missing required text field" }
+*/
+//CUSTOME MILLEWHARE/HANDLE FUNCTIONS OR INPORTS OF
+
+
+//MIDDLE WARE
+server.use(express.json());
+server.use(logger);
+
+//ROUTES
+server.get('/', logger, (req, res) => {
+  res.send(`<h2>Let's write some middleware! And I hate this HTML responce</h2>`);
 });
 
-//custom middleware
+//ENDPOINTS
 
-function logger(req, res, next) {}
 
 module.exports = server;
